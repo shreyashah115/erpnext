58f80797d9ce59e7d63b7395f60dcb61fd026eae
Shreya <shreyashah115@gmail.com>
2018-04-08 16:42:05 +0530

    expense approver child table

--- erpnext/hr/doctype/department/department.js
+++ erpnext/hr/doctype/department/department.js
@@ -2,6 +2,25 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Department', {
+
+	onload: function() {
+		this.frm.set_query("leave_approver", "leave_approvers", function(doc) {
+			return {
+				query:"erpnext.hr.doctype.employee_leave_approver.employee_leave_approver.get_approvers",
+				filters:{
+					user: doc.user_id
+				}
+			}
+		});
+		this.frm.set_query("expense_approver", "expense_approvers", function(doc) {
+			return {
+				query:"erpnext.hr.doctype.employee_expense_approver.employee_expense_approver.get_approvers",
+				filters:{
+					user: doc.user_id
+				}
+			}
+		});
+	},
 	refresh: function(frm) {
 
 	}
