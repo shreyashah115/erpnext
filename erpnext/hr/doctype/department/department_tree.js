67aea53850dc842479d777fdc521901c21f61afb
Shreya <shreyashah115@gmail.com>
2018-04-09 12:47:54 +0530

    patch to set lft rgt

--- erpnext/hr/doctype/department/department_tree.js
+++ erpnext/hr/doctype/department/department_tree.js
@@ -1,3 +1,7 @@
 frappe.treeview_settings["Department"] = {
 	ignore_fields:["parent_department"]
+<<<<<<< HEAD
 }
+=======
+};
+>>>>>>> patch to set lft rgt
--- erpnext/patches/v11_0/update_department_lft_rgt.py
+++ erpnext/patches/v11_0/update_department_lft_rgt.py
@@ -6,14 +6,12 @@ def execute():
 	""" assign lft and rgt appropriately """
 	frappe.reload_doc("hr", "doctype", "department")
 
-	if not frappe.db.exists("Department", _('All Departments')):
+	if not frappe.db.exists("Department", "All Departments"):
 		frappe.get_doc({
 			'doctype': 'Department',
 			'department_name': _('All Departments'),
 			'is_group': 1
 		}).insert(ignore_permissions=True)
-
-	frappe.db.sql("""update `tabDepartment` set parent_department = '{0}'
-		where is_group = 0""".format(_('All Departments')))
-
+	frappe.db.sql("""update `tabDepartment` set parent_department = "All Departments"
+		where is_group = 0""")
 	rebuild_tree("Department", "parent_department")
